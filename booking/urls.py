452c047d25ac1dfd75da80174ad5d2240b04a30a
wajidsarshaar <wajidsarshaar07@gmail.com>
2023-03-08 11:29:37 +0530

    from master to main branch

--- booking/urls.py
+++ booking/urls.py
@@ -5,12 +5,14 @@ urlpatterns = [
     path('logout', views.logout_, name='logout'),
     path('seats<int:id>',views.display_seats,name = 'seats'),
     path('seat-number/<int:id>/<int:id2>', views.choose_seats, name = 'choose'),
-    path('paid', views.booked, name ='booked'),
     path('venues<int:id>', views.venues, name = 'venues'),
     path('main', views.main, name = 'main'),
     path('explore<int:id>', views.explore, name = 'explore'),
     path('search', views.search_bar, name = 'search_bar'),
     path('user',views.display, name = 'display'),
     path('create',views.userProfile, name = 'profile'),
-
+    path('submit', views.submit,name = 'submit'),
+    path('test',views.test_total,name='test'),
 ]
+
+# path('paid', views.booked, name ='booked'),
